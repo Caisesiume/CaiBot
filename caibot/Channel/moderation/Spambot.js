48a3ec171ed7fbf4b7cd72d5ed48148b68bdcf7d
Caisesiume <simonarvidsson91@gmail.com>
2022-02-17 18:54:33 +0100

    Implemented filter to prevent link shorterners

--- caibot/Channel/moderation/Spambot.js
+++ caibot/Channel/moderation/Spambot.js
@@ -1,5 +1,8 @@
 const {ModerationFilter} = require("./ModerationFilter");
 const followBotFilter = /\b(b *i *g *f *o *l *l *o *w *s *(\.|\,) *c *o *m)+/gmi;
+const linkFilter1 = /\b(c*u*t*t(\.|\,) *(l*y*\/))+/gmi;
+const linkFilter2 = /\b(b*i*t(\.|\,) *(l*y*\/))+/gmi;
+const linkFilter3 = /\b(t*(\.|\,) *(l*y*\/))+/gmi;
 class Spambot extends ModerationFilter{
     reason;
     constructor(filterType,description,enabled, ban, timeoutLength) {
@@ -10,6 +13,15 @@ class Spambot extends ModerationFilter{
         if (!(msg.userInfo.isSubscriber || msg.userInfo.isVip)) {
             if (followBotFilter.exec(message)) {
                 this.reason = `Auto detect [botSpam] | Banned for: ${message}`;
+                return [true, this.getActionType(), this.reason];
+            } else if (linkFilter1.exec(message)) {
+                this.reason = `Auto detected [SuspiciousLink] | Banned for: ${message}`;
+                return [true, this.getActionType(), this.reason];
+            } else if (linkFilter2.exec(message)) {
+                this.reason = `Auto detected [SuspiciousLink] | Banned for: ${message}`;
+                return [true, this.getActionType(), this.reason];
+            } else if (linkFilter3.exec(message)) {
+                this.reason = `Auto detected [SuspiciousLink] | Banned for: ${message}`;
                 return [true, this.getActionType(), this.reason];
             }
         }
