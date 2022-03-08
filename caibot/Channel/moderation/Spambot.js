268b192416b0c4514bbb99b08f3f7c91f5c76be4
Caisesiume <simonarvidsson91@gmail.com>
2022-03-08 01:28:01 +0100

    added variation of spam

--- caibot/Channel/moderation/Spambot.js
+++ caibot/Channel/moderation/Spambot.js
@@ -1,9 +1,12 @@
 const {ModerationFilter} = require("./ModerationFilter");
 const followBotFilter = /\b(b *i *g *f *o *l *l *o *w *s *(\.|\,) *c *o *m)+/gmi;
 const followBotFilter2 =  /\b(y *o *u *r *f *o *l *l *o *w *z *(\.|\,) *c *o *m)+/gmi;
+const followBotFilter3 =  /\b(v *i *e *w *e *r *s *(\.|\,) *s *h *o *p)+/gmi;
 const linkFilter1 = /\b(c*u*t*t(\.|\,) *(l*y*\/))+/gmi;
-const linkFilter2 = /\b(b*i*t(\.|\,) *(l*y*\/))+/gmi;
-const linkFilter3 = /\b(t*(\.|\,) *(l*y*\/))+/gmi;
+const linkFilter2 = /\b(b*i*t(\.|\,)+([a-z]+)+\/)+/gmi;
+const linkFilter3 = /\b((t|o *w)+(\.|\,) *(ly|cc|co)\/)+/gmi;
+const linkFilter4 = /\b(t *i *n *y *(u* *r *l)? *(\.|\,) *(c *c|l *y|c *o *m)\/)+/gmi;
+const linkFilter5 = /\b(s *h *o *r *t *u* *r *l *(\.|\,) *a *t\/)+/gmi;
 class Spambot extends ModerationFilter{
     reason;
     constructor(filterType,description,enabled, ban, timeoutLength) {
@@ -18,6 +21,9 @@ class Spambot extends ModerationFilter{
             } else if (followBotFilter2.exec(message)) {
                 this.reason = `Auto detect [botSpam] | Banned for: ${message}`;
                 return [true, this.getActionType(), this.reason];
+            } else if (followBotFilter3.exec(message)) {
+                this.reason = `Auto detect [botSpam] | Banned for: ${message}`;
+                return [true, this.getActionType(), this.reason];
             } else if (linkFilter1.exec(message)) {
                 this.reason = `Auto detected [SuspiciousLink] | Banned for: ${message}`;
                 return [true, this.getActionType(), this.reason];
@@ -25,6 +31,12 @@ class Spambot extends ModerationFilter{
                 this.reason = `Auto detected [SuspiciousLink] | Banned for: ${message}`;
                 return [true, this.getActionType(), this.reason];
             } else if (linkFilter3.exec(message)) {
+                this.reason = `Auto detected [SuspiciousLink] | Banned for: ${message}`;
+                return [true, this.getActionType(), this.reason];
+            } else if (linkFilter4.exec(message)) {
+                this.reason = `Auto detected [SuspiciousLink] | Banned for: ${message}`;
+                return [true, this.getActionType(), this.reason];
+            } else if (linkFilter5.exec(message)) {
                 this.reason = `Auto detected [SuspiciousLink] | Banned for: ${message}`;
                 return [true, this.getActionType(), this.reason];
             }
